15ce1a6c02a34144ac61f997f6d1402e8e6f7c09
JcsnP <jcsnpch@gmail.com>
2022-12-09 03:30:45 +0700

    Update LeaderboardScreen.js
    fetching user's paces from database

--- src/screens/LeaderboardScreen.js
+++ src/screens/LeaderboardScreen.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
-import { View, Text, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, ScrollView, ActivityIndicator } from "react-native";
+import axios from "axios";
+import { MYPACE_API } from '@env';
 
 // import style
 import styles from '../styles';
@@ -11,15 +13,34 @@ import UserCard from "../components/Leaderboard/UserCard";
 import users from "../data/users";
 
 export default function LeaderboardScreen() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [leaderboard, setLeaderboard] = useState([]);
+  useEffect(() => {
+    const fetchLeaderboard = async() => {
+      const response = await axios.get(`${MYPACE_API}/leaderboard`);
+      if(response.data.status === 200) {
+        setLeaderboard(response.data.result);
+        setIsLoaded(true);
+      }
+    }
+    fetchLeaderboard();
+  }, []);
   return(
     <View style={styles.container}>
       <Text style={styles.title}>Leaderboard</Text>
       <Text style={{color: '#D70040', fontSize: 30, textTransform: 'uppercase', fontWeight: '800', textAlign: 'right'}}>Total</Text>
       <ScrollView>
         {
-          users.map((user, key) => (
-            <UserCard name={user.name} paces={user.paces} key={key} />
-          ))
+          !isLoaded && (
+            <ActivityIndicator />
+          )
+        }
+        {
+          isLoaded && (
+            leaderboard.map((user, key) => (
+              <UserCard name={user.userId} paces={user.details.paces} key={key} />
+            ))
+          )
         }
       </ScrollView>
     </View>
