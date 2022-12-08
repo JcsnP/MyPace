bf7b254ca3c7387aff93ab6248784b140897de06
JcsnP <jcsnpch@gmail.com>
2022-12-09 03:02:43 +0700

    fetching badges from database in BadgesScreen :alien:

--- src/screens/BadgesScreen.js
+++ src/screens/BadgesScreen.js
@@ -1,28 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, SafeAreaView, Text, Image, StyleSheet, ScrollView } from "react-native";
+import axios from "axios";
+
+import { MYPACE_API } from '@env';
 
 // import style
 import styles from "../styles";
 
-// import data
-import badges from "../data/badges";
+// import data [mockup data]
+// import badges from "../data/badges";
 
 export default function BadgesScreen() {
+  const [allBadges, setAllBadges] = useState([]);
+  useEffect(() => {
+    const fetchAllBadges = async() => {
+      const response = await axios.get(`${MYPACE_API}/badges`);
+      if(response.data.status === 200) {
+        setAllBadges(response.data.badges);
+      }
+    }
+
+    fetchAllBadges();
+  }, []);
   return(
     <SafeAreaView style={styles.container}>
       <Text style={styles.settingTitle}>Badges</Text>
       <ScrollView
         showsVerticalScrollIndicator={false}
       >
-        <View style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between'}}>
+        <View style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around'}}>
           {
-            badges.map((badge, key) => {
+            allBadges.map((badge, key) => {
               return(
                 <View style={customStyles.badgeBox} key={key}>
                   <Image
                     style={customStyles.badge}
                     source={{
-                      uri: badge.img
+                      uri: badge.picture
                     }}
                   />
                   <Text style={customStyles.title}>{badge.title}</Text>
@@ -39,7 +53,8 @@ export default function BadgesScreen() {
 const customStyles = StyleSheet.create({
   badge: {
     width: 100,
-    height: 100
+    height: 100,
+    alignSelf: 'center'
   },
   title: {
     color: '#FFF',
