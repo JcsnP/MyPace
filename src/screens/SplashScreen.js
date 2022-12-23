1be3b015e13455405da00ce5e09e1c01e8f83153
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2022-12-23 16:29:54 +0700

    add walking advice to the user

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,16 +1,33 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios, { Axios } from 'axios';
 import React, { useEffect, useState } from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import { MYPACE_API } from '@env';
+
 import styles from '../styles';
 
 export default function SplashScreen({navigation}) {
+  const [advice, setAdvice] = useState('');
+  const [isAdviceLading, setAdviceLoading] = useState(true);
+
   useEffect(() => {
+    const fetchAdvice = async() => {
+      try {
+        const response = await axios.get(`${MYPACE_API}/advice`);
+        setAdvice(response.data.advice)
+        setAdviceLoading(false);
+      } catch(error) {
+        console.log(error);
+      }
+    }
+
+    fetchAdvice();
     setTimeout(() => {
-       handleGetToken();
-    }, 2000);
-  })
+      handleGetToken();
+    }, 3000);
+  }, [])
 
   const handleGetToken = async() => {
     let token = await AsyncStorage.getItem('@Token');
@@ -25,6 +42,11 @@ export default function SplashScreen({navigation}) {
     <SafeAreaView style={styles.container}>
       <View style={{height: '100%', width: '100%', justifyContent: 'center'}}>
         <Text style={customStyle.title}>MYPACE</Text>
+        {
+          !isAdviceLading && (
+            <Text style={customStyle.advice}>"{advice.message}"</Text>
+          )
+        }
       </View>
     </SafeAreaView>
   );
@@ -36,5 +58,12 @@ const customStyle = StyleSheet.create({
     fontSize: 80,
     fontWeight: '800',
     textAlign: 'center'
+  },
+  advice: {
+    color: '#777',
+    textAlign: 'center',
+    marginVertical: 10,
+    fontSize: 17,
+    paddingHorizontal: 10,
   }
 });
