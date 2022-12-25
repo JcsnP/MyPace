56946c43bd000bdea266d6f19d18f284daa2f908
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2022-12-25 15:37:38 +0700

    add following system

--- src/screens/AvatarScreen.js
+++ src/screens/AvatarScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, TouchableOpacity, ActivityIndicator, ScrollView } from "react-native";
+import { Text, View, TouchableOpacity, ActivityIndicator, ScrollView, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import axios from "axios";
 
--- src/screens/FollowingScreen.js
+++ src/screens/FollowingScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
-import { View, SafeAreaView, Text, Image, StyleSheet, ScrollView, TextInput, TouchableOpacity } from "react-native";
+import { View, SafeAreaView, Text, Image, StyleSheet, ScrollView, TextInput, TouchableOpacity, Alert } from "react-native";
 import axios from "axios";
 
 import { MYPACE_API } from '@env';
@@ -11,15 +11,59 @@ import styles from "../styles";
 import TokenContext from "../contexts/TokenContext";
 
 export default function FollowingScreen() {
-  const [fiendsId, setFriensId] = useState('');
+  const [followingName, setfollowingName] = useState('');
+  const [followingList, setFollowingList] = useState([]);
   const token = useContext(TokenContext);
-  useEffect(() => {
 
+  useEffect(() => {
+    const fetchFollowings = async() => {
+      try {
+        const response = await axios.get(`${MYPACE_API}/followings`,
+          {
+            headers: {
+              "Authorization" : `Bearer ${token}`
+            }
+          }
+        );
+
+        if(response.data.status === 200) {
+          setFollowingList(response.data.followings);
+        }
+      } catch(error) {
+        console.log(error);
+      }
+    }
+
+    fetchFollowings();
   }, []);
 
-  const addFriend = () => {
-
+  const following = async() => {
+    if(followingName.length === 0) {
+      return Alert.alert('Following name is empty');
+    }
+    try {
+      const response = await axios.post(`${MYPACE_API}/followings`,
+        {
+          following_name: followingName
+        },
+        {
+          headers: {
+            "Authorization" : `Bearer ${token}`
+          }
+        }
+      );
+      console.log(response.data.status);
+    } catch(error) {
+      console.log(error);
+    }
   }
+
+  const displayFollowing = followingList.map((item, key) => (
+    <View style={customStyle.followingsCard}>
+      <Image source={{uri: item.details[0].image}} style={customStyle.followingsImage} />
+      <Text style={customStyle.followingsName}>{item.details[0].username}</Text>
+    </View>
+  ))
 
   return(
     <SafeAreaView style={styles.container}>
@@ -29,13 +73,14 @@ export default function FollowingScreen() {
           <View style={[customStyle.formGroup, {width: '75%'}]}>
             <TextInput
               style={styles.searchInput}
-              onChangeText={setFriensId}
-              value={fiendsId}
+              onChangeText={setfollowingName}
+              value={followingName}
+              clearButtonMode="always"
             />
           </View>
           <View style={[customStyle.formGroup, {width: '25%'}]}>
             <TouchableOpacity
-              onPress={addFriend}
+              onPress={following}
               style={styles.searchButton}
             >
               <Text style={styles.searchLabel}>ADD</Text>
@@ -45,12 +90,16 @@ export default function FollowingScreen() {
       </View>
 
       {/* friends list */}
-      <View style={customStyle.section}>
-        <Text style={customStyle.label}>Following Lists</Text>
-        <ScrollView vertical>
-          
-        </ScrollView>
-      </View>
+      {
+        followingList.length !== 0 && (
+          <View style={customStyle.section}>
+            <Text style={customStyle.label}>Following Lists</Text>
+            <ScrollView vertical>
+              {displayFollowing}
+            </ScrollView>
+          </View>
+        )
+      }
     </SafeAreaView>
   );
 }
@@ -69,6 +118,34 @@ const customStyle = StyleSheet.create({
   },
   section: {
     marginBottom: 20
+  },
+  followingsCard: {
+    height: 75,
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#3D3D3D",
+    borderRadius: 7,
+    backgroundColor: "#212121",
+    padding: 10,
+    color: "#A9A9A9",
+    fontWeight: "700",
+    fontSize: 20,
+    marginBottom: 15,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  followingsName: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: '25'
+  },
+  followingsImage: {
+    width: 50,
+    height: 50,
+    borderRadius: '100%',
+    backgroundColor: '#333'
   }
 });
 
